60a7c60ff893267864b8bb4a7f12a2143cba31c6
Marek Dolgos <marek@lytics.io>
2016-01-13 09:57:28 -0800

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,12 +1,9 @@
 /*
-The `dfa` package implements a deterministic finite automata for use to define stateful
-computations that are easier understood when transitions are specified explicitly. The
-API is more interested in using the DFA to clearly define stateful computation, rather
-than actually being used to recognize languages.
-*/
-package dfa
+The dfa package implements a deterministic finite automata to define stateful computations
+that are easier understood when transitions are specified explicitly. The API is more
+interested in using the DFA to clearly define stateful computation, rather than actually
+being used to recognize languages.
 
-/*
 Importing
 
     import "github.com/lytics/dfa"
@@ -16,14 +13,14 @@ Quick Example
     // Define states.
     Starting = dfa.State("starting")
     Finishing = dfa.State("finishing")
-    
+
     // Define letters.
     Done = dfa.Letter("done")
     Repeat = dfa.Letter("repeat")
-    
+
     // Error reporting is done by user code.
     var errors []error
-    
+
     // Define stateful computations.
     starting := func() dfa.Letter {
         if err := do(); err != nil {
@@ -36,14 +33,14 @@ Quick Example
     finishing := func() {
         fmt.Println("all finished")
     }
-    
+
     // Order matter, set start and terminal states first.
     d := dfa.New()
     d.SetStartState(Starting)
     d.SetTerminalStates(Finishing)
     d.SetTransition(Starting, Done, Finishing, finishing)
     d.SetTransition(Starting, Repeat, Starting, starting)
-    
+
     // Calls the given function in a new go-routine,
     // unless there were initialization errors.
     // Blocks until the DFA accepts its input or
@@ -51,9 +48,10 @@ Quick Example
     // is returned, and accepted == true if the final
     // state is a terminal state.
     final, accepted := d.Run(starting)
-    
+
     // Error handling is up to the user application.
     for _, err := range errors {
         ...
     }
 */
+package dfa
